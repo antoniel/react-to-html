2fa984070a4ea295d744307de5d7ad19eb62560b
Antoniel Magalhaes <antoniel2210@gmail.com>
2023-02-20 00:17:21 -0300

    move files

--- src/mapStyleProp.ts
+++ src/mapStyleProp.ts
@@ -1,5 +1,4 @@
-import { MappedNode } from "./types";
-import { convertToKebabCase, convertToInlineStyle, convertToPx } from "./utils";
+import { MappedNode, StyleRecord } from "./types";
 
 export const mapStyleProp = (node: MappedNode) => {
   if (!node?.props?.style) {
@@ -16,4 +15,42 @@ export const mapStyleProp = (node: MappedNode) => {
     }
   };
 };
+export const convertToInlineStyle = (style: StyleRecord): string => {
+  return Object.entries(style)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join('; ');
+}
 
+export const convertToPx = (style: StyleRecord): StyleRecord => {
+  return Object.entries(style)
+    .reduce((acc, [key, value]) => {
+      const isString = typeof value === 'string';
+      const isFlex = key === 'flex';
+      if (isString || isFlex) {
+        acc[key] = value;
+        return acc;
+      }
+
+      acc[key] = `${value}px`;
+      return acc;
+    }, {} as StyleRecord);
+}
+export function convertToKebabCase(style: StyleRecord): StyleRecord {
+  const kebabObj: StyleRecord = {};
+  for (const key in style) {
+    if (style.hasOwnProperty(key)) {
+      const kebabKey = key.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`);
+      kebabObj[kebabKey] = style[key];
+    }
+  }
+  return kebabObj;
+
+}
+
+// onStyleValues we may have  style: [{ flex: 1 }, null], we should filter and merge all the styles
+type StyleValues = string | number | undefined | null;
+type StyleRecordRaw = Record<string, StyleValues> | Array<StyleValues | StyleRecordRaw>;
+
+const mergeStyles = (style: StyleRecordRaw) => {
+  return style;
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,6 @@
 import { InputRNtoHTML, StyleRecord } from "./types";
 
-
-export function convertToKebabCase(cssObj: StyleRecord): StyleRecord {
-  const kebabObj: StyleRecord = {};
-  for (const key in cssObj) {
-    if (cssObj.hasOwnProperty(key)) {
-      const kebabKey = key.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`);
-      kebabObj[kebabKey] = cssObj[key];
-    }
-  }
-  return kebabObj;
-
-}
 export function isValidElement(object: InputRNtoHTML) {
   return typeof object === 'object' && object !== null && object.type && object.props;
 }
 
-export const convertToInlineStyle = (style: Record<string, string>): string => {
-  return Object.entries(style)
-    .map(([key, value]) => `${key}: ${value}`)
-    .join('; ');
-}
-
-export const convertToPx = (obj: StyleRecord): StyleRecord => {
-  return Object.entries(obj)
-    .reduce((acc, [key, value]) => {
-      const isString = typeof value === 'string';
-      const isFlex = key === 'flex';
-      if (isString || isFlex) {
-        acc[key] = value;
-        return acc;
-      }
-
-      acc[key] = `${value}px`;
-      return acc;
-    }, {} as StyleRecord);
-}
