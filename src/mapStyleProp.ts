9d941dc95f7bda3ceff64b1c311f1fa8100388d9
Antoniel Magalhaes <antoniel2210@gmail.com>
2023-02-21 18:28:52 -0300

    refactor: mergeStyles

--- src/mapStyleProp.ts
+++ src/mapStyleProp.ts
@@ -73,34 +73,46 @@ function convertToKebabCase(style: StyleRecord): StyleRecord {
   );
 }
 
-type StyleRecordRaw =
-  | Array<null | undefined | StyleRecord | Array<StyleRecord>>
-  | StyleRecord;
+type ArrStyleRecord = Array<
+  null | undefined | StyleRecord | Array<StyleRecord>
+>;
+type StyleRecordRaw = ArrStyleRecord | StyleRecord;
+
+export function isArray<A>(u: unknown): u is Array<A> {
+  return Array.isArray(u);
+}
+
+const isArrStyleRecord = (
+  u: StyleRecordRaw
+): E.Either<StyleRecord, ArrStyleRecord> => {
+  return isArray(u) ? E.right(u) : E.left(u);
+};
 
 export const mergeStyles = (style: StyleRecordRaw): StyleRecord => {
   return pipe(
-    style,
-    E.fromPredicate(Array.isArray, identity),
+    isArrStyleRecord(style),
     E.map(
       flow(
-        A.filter(Boolean),
+        A.filter((v): v is StyleRecord | Array<StyleRecord> => Boolean(v)),
         E.fromPredicate(A.isNonEmpty, identity),
         E.map(
-          A.reduce({}, (acc: StyleRecord, curr) => {
-            return pipe(
-              curr,
-              O.fromPredicate(Array.isArray),
-              O.fold(
-                () => ({ ...acc, ...curr }),
-                () => ({ ...acc, ...mergeStyles(curr) })
-              )
-            );
-          })
+          A.reduce<ArrStyleRecord | StyleRecord, StyleRecord>(
+            {},
+            (acc, curr) => {
+              return pipe(
+                isArrStyleRecord(curr),
+                E.fold(
+                  a => ({ ...acc, ...a }),
+                  b => ({ ...acc, ...mergeStyles(b) })
+                )
+              );
+            }
+          )
         ),
         E.getOrElse(() => ({}))
       )
     ),
-    E.getOrElse(identity)
+    E.getOrElse(a => a)
   );
 };
 
