32c1ca9a1031f7495cea1e2501fc41cab325027c
Antoniel Magalhaes <antoniel2210@gmail.com>
2023-02-21 17:12:48 -0300

    fix: test match

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,15 @@ export default defineConfig({
   test: {
     watch: false,
     globals: true,
-    environment: "node",
+    exclude: [
+      'examples/**/*.{jsx,tsx}',
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
+    environment: 'node',
     includeSource: ['src/**/*.{js,ts}'],
   },
 });
