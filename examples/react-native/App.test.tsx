05be86738278ce29c9993debd74387309524feb9
Antoniel Magalhaes <antoniel2210@gmail.com>
2023-02-21 12:43:44 -0300

    test: add test cases on exemples/native

--- examples/react-native/App.test.tsx
+++ examples/react-native/App.test.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
 import App from './App';
 import { render } from '@testing-library/react-native';
+import { fromRNtoHTML } from '@antoniel/react-native-to-jest-preview';
 
-it('Render App', () => {
-  render(<App />);
+// :🚨 to toggle between react-native-web and react-native see babel.config.js
+it.skip('Render with react-native-web', () => {
+  const { toJSON } = render(<App />);
+  expect(toJSON()).toMatchInlineSnapshot(`
+    <div
+      className="css-view-175oi2r r-alignItems-1awozwy r-backgroundColor-14lw9ot r-flex-13awgt0 r-justifyContent-1777fci"
+      dir={null}
+    >
+      <div
+        className="css-text-1rynq56"
+        dir="auto"
+      >
+        Open up App.js to start working on your app!
+      </div>
+    </div>
+  `);
+});
+it('Render with react-native', () => {
+  const { toJSON } = render(<App />);
+  expect(toJSON()).toMatchInlineSnapshot(`
+    <View
+      style={
+        Object {
+          "alignItems": "center",
+          "backgroundColor": "#fff",
+          "flex": 1,
+          "justifyContent": "center",
+        }
+      }
+    >
+      <Text>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  `);
+});
+
+it('render with react-native using "react-native-to-jest-preview" transform', () => {
+  const { toJSON } = render(<App />);
+  expect(fromRNtoHTML(toJSON())).toMatchInlineSnapshot(`
+    "<div
+      style=\\"flex: 1; background-color: #fff; align-items: center; justify-content: center\\"
+    >
+      <p>
+        Open up App.js to start working on your app!
+      </p>
+    </div>"
+  `);
 });
--- examples/react-native/babel.config.js
+++ examples/react-native/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      // To enable react-native-web just uncomment the following line
+      // [
+      //   '@antoniel/module-resolver',
+      //   {
+      //     alias: {
+      //       '^react-native$': 'react-native-web',
+      //     },
+      //   },
+      // ],
+    ],
   };
 };
